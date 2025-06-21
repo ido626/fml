3b9dd39918908cc3182916e3d0e54d07d43f3ac1
ido626 <amit.ido@campus.technion.ac.il>
2025-06-21 18:43:20 +0300

    Update ili_handler.asm

--- ili_handler.asm
+++ ili_handler.asm
@@ -1,73 +1,67 @@
 .globl my_ili_handler
+
 .text
 .align 4, 0x90
 my_ili_handler:
-    # 1) Load pointer-to-RIP from the stack, then load the RIP itself:
-    movq    (%rsp),  %rdx       # %rdx = address-of-[RIP] (לפי השערתך)
-    movq    (%rdx),  %rdx       # %rdx = actual RIP
 
-    # 2) שמירת כל הרגיסטרים הקלצים
-    pushq   %r15
-    pushq   %r14
-    pushq   %r13
-    pushq   %r12
-    pushq   %rbx
-    pushq   %rbp
-    movq    %rsp,   %rbp       # frame pointer
+	movq (%rsp), %rcx
+	movq (%rcx), %rcx
 
-    # 3) בודקים אם אופקוד דו-בתי או חד-ביתי
-    movb    (%rdx), %al
-    cmpb    $0x0F, %al
-    jne     one_byte_opcode
+  pushq %r15
+	pushq %r14
+	pushq %r13
+	pushq %r12
+	pushq %rbx
+  pushq %rbp
+	movq %rsp, %rcx 
 
-    #–––– 2-byte opcode path –––––––––
-    movb    1(%rdx),  %al
-    movzbl  %al,     %edi
-    call    what_to_do
-    test    %rax,    %rax
-    je      no_patch
+  cmpb $0x0F, %cl
+  jne one_byte_opcode
 
-    # 4) שחזור סטאק וכתובת חזרה – במקרה של תיקון
-    movq    %rbp, %rsp
-    popq    %rbp
-    popq    %rbx
-    popq    %r12
-    popq    %r13
-    popq    %r14
-    popq    %r15
-
-    # 5) הוספת 2 ל־RIP שמאוחסן בכתובת (%rsp)
-    addq    $2, (%rsp)
-    jmp     done
+  movb %dh, %al
+  movb %al, %dil
+  call what_to_do
+  cmp $0, %rax
+  je no_patch
+  
+  movq %rax, %rdi
+  movq %rbp, %rsp
+	popq %rbp
+	popq %rbx
+	popq %r12
+	popq %r13
+	popq %r14
+	popq %r15
+  addq $2, (%rsp)
+  jmp done
 
 one_byte_opcode:
-    #–––– 1-byte opcode path –––––––––
-    movzbl  (%rdx), %edi
-    call    what_to_do
-    test    %rax,   %rax
-    je      no_patch
+  movb %dl, %dil
+  call what_to_do
+  cmp $0, %rax
+  je no_patch
 
-    movq    %rbp, %rsp
-    popq    %rbp
-    popq    %rbx
-    popq    %r12
-    popq    %r13
-    popq    %r14
-    popq    %r15
-
-    addq    $1, (%rsp)
-    jmp     done
+  movq %rax, %rdi
+  movq %rbp, %rsp
+	popq %rbp
+  popq %rbx
+	popq %r12
+	popq %r13
+	popq %r14
+	popq %r15
+  addq $1, (%rsp)
+  jmp done
 
 no_patch:
-    #–––– ללא תיקון, דילוג ל־old handler –––––––––
-    movq    %rbp, %rsp
-    popq    %rbp
-    popq    %rbx
-    popq    %r12
-    popq    %r13
-    popq    %r14
-    popq    %r15
-    jmp    *old_ili_handler
+  movq %rbp, %rsp
+	popq %rbp
+  popq %rbx
+	popq %r12
+	popq %r13
+	popq %r14
+	popq %r15
+  
+  jmp * old_ili_handler
 
 done:
-    iretq
+  iretq
