1cadf74aee365b11cd11ad9b285d9adae4019cf5
ido626 <amit.ido@campus.technion.ac.il>
2025-06-21 16:35:21 +0300

    Add files via upload

---
+++ ili_handler.asm
@@ -0,0 +1,34 @@
+# ili_handler.asm
+    .globl my_ili_handler
+    .extern old_ili_handler
+
+    .text
+    .align 4, 0x90
+my_ili_handler:
+    mov    (%rsp), %rdi
+
+    movb   (%rdi), %al
+    cmpb   $0x0F, %al
+    jne    one_byte_opcode
+
+    movb   1(%rdi), %al
+    movzbl %al, %edi
+    call   what_to_do
+    test   %rax, %rax
+    je     no_patch
+    addq   $2, (%rsp)
+    jmp    done
+
+one_byte_opcode:
+    movzbl %al, %edi
+    call   what_to_do
+    test   %rax, %rax
+    je     no_patch
+    addq   $1, (%rsp)
+    jmp    done
+
+no_patch:
+    jmp    *old_ili_handler(%rip)
+
+done:
+    iretq
