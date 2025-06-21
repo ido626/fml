b820cb49da270f5ea2dbc07f682443a308fb3d11
ido626 <amit.ido@campus.technion.ac.il>
2025-06-21 18:21:46 +0300

    Update ili_handler.asm

--- ili_handler.asm
+++ ili_handler.asm
@@ -1,33 +1,65 @@
-# ili_handler.asm
-    .globl my_ili_handler
+.globl my_ili_handler
 
-    .text
-    .align 4, 0x90
+.text
+.align 4, 0x90
 my_ili_handler:
-    mov    (%rsp), %rdi
+  ####### Some smart student's code here #######
+    movq    (%rsp), %rdx
 
-    movb   (%rdi), %al
+    pushq   %r15
+    pushq   %r14
+    pushq   %r13
+    pushq   %r12
+    pushq   %rbx
+    pushq   %rbp
+    movq    %rsp, %rbp 
+
+    movb   (%rdx), %al
     cmpb   $0x0F, %al
     jne    one_byte_opcode
 
-    movb   1(%rdi), %al
+    movb   1(%rdx), %al
     movzbl %al, %edi
     call   what_to_do
     test   %rax, %rax
     je     no_patch
+
+
+    movq    %rbp, %rsp
+    popq    %rbp
+    popq    %rbx
+    popq    %r12
+    popq    %r13
+    popq    %r14
+    popq    %r15
     addq   $2, (%rsp)
     jmp    done
 
 one_byte_opcode:
-    movzbl %al, %edi
+    movzbl (%rdx), %edi
     call   what_to_do
     test   %rax, %rax
     je     no_patch
+
+    movq    %rbp, %rsp
+    popq    %rbp
+    popq    %rbx
+    popq    %r12
+    popq    %r13
+    popq    %r14
+    popq    %r15
     addq   $1, (%rsp)
     jmp    done
 
 no_patch:
-    jmp    *old_ili_handler(%rip)
+    movq    %rbp, %rsp
+    popq    %rbp
+    popq    %rbx
+    popq    %r12
+    popq    %r13
+    popq    %r14
+    popq    %r15
+    jmp    * old_ili_handler
 
 done:
-    iretq
+  iretq
